7bfaafb7e57eca3f56cc8150b2472feb96c6bdee
Syntu <66807482+Syntu@users.noreply.github.com>
2025-01-07 07:55:05 +0545

    Add files via upload

---
+++ render-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install Chrome Browser
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
+
+# Install ChromeDriver
+CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
+wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION.0/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/local/bin/
+# Install Python Dependencies
+pip install -r requirements.txt
