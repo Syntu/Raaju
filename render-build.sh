d3610efc09d895861485eaaa48e96bbc8e0fdd93
Syntu <66807482+Syntu@users.noreply.github.com>
2025-01-08 17:44:46 +0545

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+# Install necessary libraries
+apt-get update
+apt-get install -y wget unzip curl gnupg
+
 # Install Chrome Browser
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install ./google-chrome-stable_current_amd64.deb -y
+dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
 
-# Install ChromeDriver
-CHROME_VERSION=$(google-chrome --version | cut -d ' ' -f 3 | cut -d '.' -f 1)
-wget https://chromedriver.storage.googleapis.com/${CHROME_VERSION}.0/chromedriver_linux64.zip
-unzip chromedriver_linux64.zip
-sudo mv chromedriver /usr/local/bin/
-
-# Install Python Dependencies (requirements.txt)
-pip install -r requirements.txt
+# Verify Chrome installation
+google-chrome --version
