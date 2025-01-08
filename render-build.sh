ca06c4bc213182012c49704f1ba81c8830ac2b28
Syntu <66807482+Syntu@users.noreply.github.com>
2025-01-08 17:59:16 +0545

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Install necessary libraries
+# Update the package list and install Chromium and Chromium Driver
 apt-get update
-apt-get install -y wget unzip curl gnupg
+apt-get install -y chromium chromium-driver
 
-# Install Chrome Browser
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb || apt-get -f install -y
-
-# Verify Chrome installation
-google-chrome --version
+# Verify installation
+chromium --version
+chromium-driver --version
